211f1431d9645058ae5e85e5d481b3cdc285ba5e
João Pedro <joaopedroaat@outlook.com>
2024-12-27 17:18:10 -0300

    Remove hello world

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/joaopedroaat/go-do/internal/models"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
-
 	models.AddTask("Take dog for a walk")
 	models.AddTask("Wash the dishes")
 
