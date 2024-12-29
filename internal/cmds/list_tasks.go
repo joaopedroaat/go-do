7904e548a84cdc7dd839c617167b729b541aa3b3
João Pedro <joaopedroaat@outlook.com>
2024-12-28 21:06:42 -0300

    Create a flag to list only completed tasks

--- internal/cmds/list_tasks.go
+++ internal/cmds/list_tasks.go
@@ -1,6 +1,7 @@
 package cmds
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joaopedroaat/go-do/internal/services"
@@ -8,12 +9,27 @@ import (
 )
 
 func ListTasks(taskService services.TaskService) *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "List all todos",
 		Args:  cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
+			completed, err := cmd.Flags().GetBool("completed")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			if completed {
+				fmt.Println("Completed tasks:")
+				return
+			}
+
 			taskService.WriteTasks(os.Stdout)
 		},
 	}
+
+	cmd.Flags().Bool("completed", true, "List only completed tasks")
+
+	return cmd
 }
